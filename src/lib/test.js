6f041551aa2c4945f290fd350859dced0a9544e1
theflower86 <theflower86@mail.ru>
2018-10-11 01:45:57 +0300

    HW1: eslint style fixed

--- src/lib/test.js
+++ src/lib/test.js
@@ -1,10 +1,9 @@
-const say = function (name) {
-  //return `Hello ${name}`;
-  var div = document.createElement('div');
-  div.className = "alert alert-success";
-  div.innerHTML = `Hello ${name}`;
-  document.body.style.backgroundColor = 'green';
-  document.body.appendChild(div);
+const say = function sayOnTheScreen(name) {
+    const div = document.createElement('div');
+    div.className = 'alert alert-success';
+    div.innerHTML = `Hello ${name}`;
+    document.body.style.backgroundColor = 'green';
+    document.body.appendChild(div);
 };
 
 export default say;
