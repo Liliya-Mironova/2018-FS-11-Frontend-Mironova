1819d04a7d93dc56213fb472499d2b13a55d8ba2
theflower86 <theflower86@mail.ru>
2018-10-11 20:50:27 +0300

    HW1: styles.css added

--- src/lib/test.js
+++ src/lib/test.js
@@ -1,8 +1,6 @@
 const say = function sayOnTheScreen(name) {
-    const div = document.createElement('div');
-    div.className = 'alert alert-success';
+    let div = document.createElement('div');
     div.innerHTML = `Hello ${name}`;
-    document.body.style.backgroundColor = 'green';
     document.body.appendChild(div);
 };
 
