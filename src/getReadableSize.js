a314be229a63305c24a582c1dc20e14f37d0b9b2
theflower86 <theflower86@mail.ru>
2018-10-18 23:52:44 +0300

    HW2: a bug in getReadableSize fixed

--- src/getReadableSize.js
+++ src/getReadableSize.js
@@ -2,14 +2,12 @@ function getReadableSize(size) {
   const arr = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB'];
   let newSize = size;
   for (const item in arr) {
-    if (Object.prototype.hasOwnProperty.call(item, arr)) {
       if (newSize < 1024) {
-        return `${newSize} ${arr[item]}`;
+        return `${Math.ceil(newSize)} ${arr[item]}`;
       }
-      newSize = Math.floor(newSize / 1024);
-    }
+      newSize /= 1024;
   }
-  return `${newSize} B`;
+  return `${Math.ceil(newSize)} B`;
 }
 
 console.log(getReadableSize(2048));
