f8eacc9762e8d032a0f19771a2f426d92b747e48
theflower86 <theflower86@mail.ru>
2018-11-01 23:16:41 +0300

    HW3: file preview added, styles changed, clearing of the input form after submit added, D&D error fixed

--- src/lib/components/message-form/index.js
+++ src/lib/components/message-form/index.js
@@ -5,12 +5,12 @@ const slotName = 'message-input';
 
 const template = `
 	<style>${shadowStyles.toString()}</style>
-	<form>
+	<form id="drop_zone">
 		<input type="file"/>
-		<div id="drop_zone">Drop</div>
 		<button id="location">Geolocation</button>
 		<div id="coords"></div>
 		<ul class="result"></ul>
+		<img id="clip" src="/home/katze/2018-FS-11-Frontend-Mironova/src/lib/components/message-form/clip.png">
 		<form-input name="message_text" placeholder="Введите сообщение" slot="message-input">
 			<span slot="icon"></span>
 		</form-input>
@@ -44,6 +44,7 @@ class MessageForm extends HTMLElement {
 		var dropZone = this.shadowRoot.getElementById('drop_zone');
 		var locationButton = this.shadowRoot.getElementById('location');
 		var output = this.shadowRoot.getElementById('coords');
+		const pos = '';
 		this._elements = {
 			form: form,
 			message: message,
@@ -69,9 +70,10 @@ class MessageForm extends HTMLElement {
 	}
 
 	_onClick (event) {
-		event.stopPropagation();
 		event.preventDefault();
 		var output = this._elements.output;
+		var li = document.createElement('li');
+		var msg = this._elements.message;
 
 		if (!navigator.geolocation) {
 			output.innerText = "Geolocation is not supported by your browser";
@@ -79,9 +81,8 @@ class MessageForm extends HTMLElement {
   		}
 
 		function success(position) {
-			var latitude  = position.coords.latitude;
-			var longitude = position.coords.longitude;
-			output.innerText = `${position.coords.latitude}, ${position.coords.longitude}`;
+			li.innerText = `${position.coords.latitude}, ${position.coords.longitude}`;
+			msg.appendChild(li);
 		};
 
 		function error() {
@@ -99,41 +100,60 @@ class MessageForm extends HTMLElement {
 		if (li.innerText.length > 2) {
 			this._elements.message.appendChild(li);
 		}
-		/*this._elements.form._elements.input.value = "";*/
 
 		event.preventDefault();
 		return false;
 	}
 
 	_onFileSelect (event) {
-		event.stopPropagation();
 		event.preventDefault();
+		var msg = this._elements.message;
 
 		var files = event.target.files;
+		var file_type = files[0].type;
 
-		if (files[0].type.match("image\/.*")) {
-			var img = document.createElement('img');
-			img.src = files[0].value;
-			this._elements.message.appendChild(img);
-		} else {
-			var li = document.createElement('li');
-			li.innerText=`${files[0].name}, ${files[0].type}, ${getReadableSize(files[0].size)}`;
-			this._elements.message.appendChild(li);
-		}
+		var reader = new FileReader();
 
+		reader.onload = function(){
+			if (file_type.startsWith('image/')) {
+				var img = document.createElement('img');
+				img.id = 'preview';
+				img.src = reader.result;
+				msg.appendChild(img);
+			} else {
+				var li = document.createElement('li');
+				li.innerText=`${files[0].name}, ${files[0].type}, ${getReadableSize(files[0].size)}`;
+				msg.appendChild(li);
+			}
+    	};
+
+		reader.readAsDataURL(event.target.files[0]);
 		return false;
 	}
 
 	_onDragOver (event) {
-		event.stopPropagation();
 		event.preventDefault();
-
-		var dt = event.dataTransfer;
-		var files = dt.files;
-		var li = document.createElement('li');
-		li.innerText=`${files[0].name}, ${files[0].type}, ${getReadableSize(files[0].size)}`;
-		this._elements.message.appendChild(li);
-		//event.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+			 
+		var files = event.dataTransfer.files;
+		
+		for (var i = 0; i < files.length; i++) {
+		    var file = files[i];
+		    
+		    if (file.type.startsWith('image/')) {
+			    var img = document.createElement("img");
+			    img.id = 'preview';
+			    img.file = file;
+			    this._elements.message.appendChild(img);
+			    
+			    var reader = new FileReader();
+			    reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(img);
+			    reader.readAsDataURL(file);
+			} else {
+				var li = document.createElement('li');
+				li.innerText=`${files[0].name}, ${files[0].type}, ${getReadableSize(files[0].size)}`;
+				this._elements.message.appendChild(li);
+			}
+		}
 	}
 
 	_onKeyPress (event) {
