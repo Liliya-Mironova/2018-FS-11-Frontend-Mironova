043853ddbed2542a0b53b1ac801adfce6bbc954e
theflower86 <theflower86@mail.ru>
2018-11-12 12:34:42 +0300

    HW5: app lauchingin app/create/index.* redone

--- src/app/create/index.js
+++ src/app/create/index.js
@@ -1,2 +1,9 @@
-import {FormInput} from '../../lib/components/form/-input';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import {MessageForm} from '../../lib/components/message-form';
+
+ReactDOM.render(
+    <MessageForm action="/" />, 
+    document.getElementById('app')
+);
